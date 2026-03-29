57872b536f2ecd69c20fb8848572161b781883ef
JeanBackEndOnly <145352326+JeanBackEndOnly@users.noreply.github.com>
2026-03-29 19:51:31 +0800

    tab icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
